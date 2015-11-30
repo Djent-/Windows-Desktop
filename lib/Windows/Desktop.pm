f71aa66c31cc27e00b25be98c57e23a9a5532ba7
PH <djent.git@cock.li>
2015-11-30 15:14:31 -0500

    Trying to get NativeCall to work with SystemParametersInfo

--- lib/Windows/Desktop.pm
+++ lib/Windows/Desktop.pm
@@ -1,23 +1,24 @@
 # Copyright 2015 Patrick Hurd
 
 use v6;
-use warnings;
 use strict;
 use NativeCall;
 
 unit module Windows::Desktop;
 
 # Constants used by SystemParametersInfo
-my $!SPI_SETDESKWALLPAPER  = 20;
-my $!SPIF_UPDATEANDSENDINI = 3;
+my $SPI_SETDESKWALLPAPER  = 20;
+my $SPIF_UPDATEANDSENDINI = 3;
 
 # Might need to mess with integer size and whether P translates to Pointer[void]
-sub SystemParametersInfo(int32, int32, Pointer[void], int32)
+sub SystemParametersInfo(int32, int32, Pointer[int32], int32)
 	returns int32
 	is native("user32")
 	{ * }
 	
-sub SetWallpaper (Str $filename) is export {
+sub SetWallpaper ($filename) is export {
 	# TODO: check whether provided filename exists
 	SystemParametersInfo($SPI_SETDESKWALLPAPER, 0, $filename, $SPIF_UPDATEANDSENDINI);
 }
+
+SetWallpaper("E:/Wallpapers/1305281760696.jpg");
